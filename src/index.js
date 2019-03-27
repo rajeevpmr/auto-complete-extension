b9dce120e95c1d9637b2afabccf62aaa8c1b3d9b
RR <rrmachandran2@sapient.com>
2019-03-27 17:11:32 -0600

    Updated with contentful SDK

--- src/index.js
+++ src/index.js
@@ -1,11 +1,79 @@
 import React from 'react';
+import Select from 'react-select';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { TextInput } from '@contentful/forma-36-react-components';
+import { init } from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  static propTypes = {
+    sdk: PropTypes.object.isRequired,
+  };
+
+  detachExternalChangeHandler = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.sdk.field.getValue(),
+    };
+  }
+
+  componentDidMount() {
+    this.props.sdk.window.startAutoResizer();
+
+    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
+      this.onExternalChange
+    );
+  }
+
+  componentWillUnmount() {
+    if (this.detachExternalChangeHandler) {
+      this.detachExternalChangeHandler();
+    }
+  }
+
+  onExternalChange = value => {
+    this.setState({ value });
+  };
+
+  onChange = e => {
+    const value = e.currentTarget.value;
+    this.setState({ value });
+    if (value) {
+      this.props.sdk.field.setValue(value);
+    } else {
+      this.props.sdk.field.removeValue();
+    }
+  };
+
+  render() {
+    const { selectedOption } = this.state.value;
+
+    return (
+      <Select
+        value={selectedOption}
+        onChange={this.onChange}
+        options={options}
+      />
+    );
+  }
+}
+
+init(sdk => {
+  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+});
+
+// Enabling hot reload
+if (module.hot) {
+  module.hot.accept();
+}
